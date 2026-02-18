cffc8ee09b8053c8937b29fbc2c9700d737f24a5
dhanush290707 <dhanush290707@gmail.com>
2026-02-18 08:37:29 +0530

    fix: add basename to BrowserRouter for GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/ConsensusMind">
             <AuthProvider>
                 <DecisionProvider>
                     <App />
